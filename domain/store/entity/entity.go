c0fc204b0035f33d7349b81170dedd4a08e35614
Fazar Rahman <fazar.rahman@gmail.com>
2023-12-06 11:20:48 +0700

    Define data types to store migration

--- domain/store/entity/entity.go
+++ domain/store/entity/entity.go
@@ -8,24 +8,24 @@ import (
 )
 
 type Store struct {
-	Id          uuid.UUID  `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
-	UserId      uuid.UUID  `gorm:"type:uuid;not null"`
-	Name        string     `gorm:"type:varchar(255);not null"`
-	Address     string     `gorm:"type:text;null"`
-	MapLocation string     `gorm:"type:varchar(255);null"`
-	LogoImageId *uuid.UUID `gorm:"type:varchar(255);null"`
-	Status      string     `gorm:"type:varchar(50);not null"`
-	IsActive    bool       `gorm:"type:bool;not null"`
-	CreatedAt   time.Time  `gorm:"type:timestamptz;not null"`
-	CreatedBy   uuid.UUID  `gorm:"type:uuid;not null"`
-	UpdatedAt   *time.Time `gorm:"type:timestamptz;null"`
-	UpdatedBy   *uuid.UUID `gorm:"type:uuid;null"`
+	Id          uuid.UUID     `gorm:"primaryKey;type:uuid;default:uuid_generate_v4()"`
+	UserId      uuid.UUID     `gorm:"type:uuid;not null"`
+	Name        string        `gorm:"type:varchar(255);not null;unique"`
+	Address     string        `gorm:"type:text;null"`
+	MapLocation string        `gorm:"type:varchar(255);null"`
+	LogoImageId uuid.NullUUID `gorm:"type:varchar(255);null"`
+	Status      string        `gorm:"type:varchar(50);not null"`
+	IsActive    bool          `gorm:"type:bool;not null;default:TRUE"`
+	CreatedAt   time.Time     `gorm:"type:timestamptz;not null;default:CURRENT_TIMESTAMP"`
+	CreatedBy   uuid.UUID     `gorm:"type:uuid;not null"`
+	UpdatedAt   *time.Time    `gorm:"type:timestamptz;null"`
+	UpdatedBy   uuid.NullUUID `gorm:"type:uuid;null"`
 }
 
 func (s *Store) ToProto() *pb.Store {
 	var logoImageId string
-	if s.LogoImageId != nil {
-		logoImageId = s.LogoImageId.String()
+	if s.LogoImageId.Valid {
+		logoImageId = s.LogoImageId.UUID.String()
 	}
 	return &pb.Store{
 		Id:          s.Id.String(),
