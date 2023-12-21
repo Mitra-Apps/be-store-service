cff01ecb96c6edae8a6ee79f94422fb3b8fd9ffd
Risyal Ambiyansyah <v-risyal.ambiyansyah@btpnsyariah.com>
2023-12-21 10:55:26 +0700

    🔧 fix(storage.go): remove unnecessary comments and improve code readability 🚀 feat(storage.go): remove hardcoded useSSL value and use environment variable instead for better configurability 🔧 fix(storage.go): remove unnecessary comments and improve code readability 🔧 fix(storage.go): remove unnecessary comments and improve code readability

--- domain/store/repository/storage/storage.go
+++ domain/store/repository/storage/storage.go
@@ -21,16 +21,14 @@ type storage struct {
 	bucket string
 }
 
-// New creates a new instance of the Storage interface.
 func New() repository.Storage {
 	endpoint := os.Getenv("MINIO_ENDPOINT")
 	accessKey := os.Getenv("MINIO_ACCESS_KEY")
 	secretKey := os.Getenv("MINIO_SECRET_KEY")
 	bucketName := os.Getenv("MINIO_BUCKET_NAME")
 
-	useSSL := false // Set to true if your MinIO server uses SSL/TLS
+	useSSL := false
 
-	// Initialize a new MinIO client object
 	minioClient, err := minio.New(endpoint, &minio.Options{
 		Creds:  credentials.NewStaticV4(accessKey, secretKey, ""),
 		Secure: useSSL,
@@ -39,7 +37,6 @@ func New() repository.Storage {
 		logrus.Fatalln("Error creating MinIO client:", err)
 	}
 
-	// Create the bucket if it doesn't exist
 	exists, err := minioClient.BucketExists(context.Background(), bucketName)
 	if err != nil {
 		logrus.Fatalln("Error checking if bucket exists:", err)
@@ -60,17 +57,14 @@ func New() repository.Storage {
 func (s *storage) UploadImage(ctx context.Context, image string) (string, error) {
 	objectName := uuid.New().String()
 
-	// Decode base64-encoded image string
 	decodedImage, err := base64.StdEncoding.DecodeString(image)
 	if err != nil {
 		fmt.Println("Error decoding base64 image:", err)
 		return "", err
 	}
 
-	// Determine file type using http.DetectContentType
 	fileType := http.DetectContentType(decodedImage)
 
-	// Append file extension based on detected file type
 	fileExtension, err := mime.ExtensionsByType(fileType)
 	if err != nil || len(fileExtension) == 0 {
 		fmt.Println("Error determining file extension:", err)
@@ -79,13 +73,11 @@ func (s *storage) UploadImage(ctx context.Context, image string) (string, error)
 
 	objectName += fileExtension[0]
 
-	// Use PutObjectWithContext to upload the image
 	_, err = s.client.PutObject(ctx, s.bucket, objectName, bytes.NewReader(decodedImage), int64(len(decodedImage)), minio.PutObjectOptions{})
 	if err != nil {
 		fmt.Println("Error uploading image:", err)
 		return "", err
 	}
 
-	// Return the URL of the uploaded image
 	return fmt.Sprintf("http://%s/%s/%s", os.Getenv("MINIO_ENDPOINT"), s.bucket, objectName), nil
 }
