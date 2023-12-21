60f6e621f10a223df375e89180200c5054fce6f9
Risyal Ambiyansyah <v-risyal.ambiyansyah@btpnsyariah.com>
2023-12-21 10:54:42 +0700

    🔧 refactor(postgres.go): remove unnecessary comments and improve code readability in CreateStore method 🔧 refactor(postgres.go): remove unnecessary comments and improve code readability in UpdateStore method 🔧 refactor(postgres.go): remove unnecessary comments and improve code readability in updateStoreHours method 🔧 refactor(postgres.go): remove unnecessary comments and improve code readability in updateStoreImages method 🔧 refactor(postgres.go): remove unnecessary comments and improve code readability in updateStoreTags method

--- domain/store/repository/postgres/postgres.go
+++ domain/store/repository/postgres/postgres.go
@@ -13,23 +13,18 @@ type postgres struct {
 	db *gorm.DB
 }
 
-// NewPostgres creates a new instance of the PostgreSQL store repository.
 func NewPostgres(db *gorm.DB) repository.StoreServiceRepository {
 	return &postgres{db}
 }
 
 func (p *postgres) CreateStore(ctx context.Context, store *entity.Store) (*entity.Store, error) {
-	// Begin the transaction
 	tx := p.db.WithContext(ctx).Begin()
 
-	// Save the store and its associations
 	if err := tx.Save(store).Error; err != nil {
-		// Rollback the transaction if there is an error
 		tx.Rollback()
 		return nil, err
 	}
 
-	// Commit the transaction
 	if err := tx.Commit().Error; err != nil {
 		return nil, err
 	}
@@ -46,7 +41,6 @@ func (p *postgres) GetStore(ctx context.Context, storeID string) (*entity.Store,
 }
 
 func (p *postgres) UpdateStore(ctx context.Context, storeID string, update *entity.Store) (*entity.Store, error) {
-	// Start transaction
 	tx := p.db.WithContext(ctx).Begin()
 
 	var existingStore entity.Store
@@ -55,7 +49,6 @@ func (p *postgres) UpdateStore(ctx context.Context, storeID string, update *enti
 		return nil, err
 	}
 
-	// Update basic store fields
 	if err := tx.Model(&existingStore).Updates(map[string]interface{}{
 		"store_name":   update.StoreName,
 		"address":      update.Address,
@@ -71,7 +64,6 @@ func (p *postgres) UpdateStore(ctx context.Context, storeID string, update *enti
 		return nil, err
 	}
 
-	// Update related entities (Hours, Images, Tags)
 	if err := p.updateStoreHours(ctx, existingStore, update.Hours); err != nil {
 		tx.Rollback()
 		return nil, err
@@ -87,7 +79,6 @@ func (p *postgres) UpdateStore(ctx context.Context, storeID string, update *enti
 		return nil, err
 	}
 
-	// Commit transaction
 	if err := tx.Commit().Error; err != nil {
 		return nil, err
 	}
@@ -97,13 +88,11 @@ func (p *postgres) UpdateStore(ctx context.Context, storeID string, update *enti
 
 func (p *postgres) updateStoreHours(ctx context.Context, existingStore entity.Store, updatedHours []entity.StoreHour) error {
 	for _, updatedHour := range updatedHours {
-		// Find the corresponding existing hour
 		var existingHour entity.StoreHour
 		if err := p.db.WithContext(ctx).Where("id = ?", updatedHour.ID).First(&existingHour).Error; err != nil {
 			return err
 		}
 
-		// Update the existing hour
 		if err := p.db.WithContext(ctx).Model(&existingHour).Updates(map[string]interface{}{
 			"day_of_week": updatedHour.DayOfWeek,
 			"open":        updatedHour.Open,
@@ -116,7 +105,6 @@ func (p *postgres) updateStoreHours(ctx context.Context, existingStore entity.St
 }
 
 func (p *postgres) updateStoreImages(ctx context.Context, existingStore entity.Store, updatedImages []entity.StoreImage) error {
-	// Delete existing images not present in the updated list
 	for _, existingImage := range existingStore.Images {
 		found := false
 		for _, updatedImage := range updatedImages {
@@ -132,9 +120,7 @@ func (p *postgres) updateStoreImages(ctx context.Context, existingStore entity.S
 		}
 	}
 
-	// Update or create updated images
 	for _, updatedImage := range updatedImages {
-		// Find the corresponding existing image
 		var existingImage entity.StoreImage
 		if updatedImage.ID.String() != "" {
 			if err := p.db.WithContext(ctx).Where("id = ?", updatedImage.ID).First(&existingImage).Error; err != nil {
@@ -142,7 +128,6 @@ func (p *postgres) updateStoreImages(ctx context.Context, existingStore entity.S
 			}
 		}
 
-		// Update or create the existing image
 		if err := p.db.WithContext(ctx).Model(&existingImage).Updates(map[string]interface{}{
 			"image_type": updatedImage.ImageType,
 			"image_url":  updatedImage.ImageURL,
@@ -154,7 +139,6 @@ func (p *postgres) updateStoreImages(ctx context.Context, existingStore entity.S
 }
 
 func (p *postgres) updateStoreTags(ctx context.Context, existingStore entity.Store, updatedTags []entity.StoreTag) error {
-	// Delete existing categories not present in the updated list
 	for _, existingCategory := range existingStore.Tags {
 		found := false
 		for _, updatedCategory := range updatedTags {
@@ -170,9 +154,7 @@ func (p *postgres) updateStoreTags(ctx context.Context, existingStore entity.Sto
 		}
 	}
 
-	// Update or create updated categories
 	for _, updatedCategory := range updatedTags {
-		// Find the corresponding existing category
 		var existingCategory entity.StoreTag
 		if updatedCategory.ID.String() != "" {
 			if err := p.db.WithContext(ctx).Where("id = ?", updatedCategory.ID).First(&existingCategory).Error; err != nil {
@@ -180,7 +162,6 @@ func (p *postgres) updateStoreTags(ctx context.Context, existingStore entity.Sto
 			}
 		}
 
-		// Update or create the existing category
 		if err := p.db.WithContext(ctx).Model(&existingCategory).Updates(map[string]interface{}{
 			"category_name": updatedCategory.TagName,
 		}).Error; err != nil {
